0c050944a678253534095cb84857f47353aa4d90
OLIVIA MCGOFFIN <orhanna14@gmail.com>
2019-10-03 17:32:40 -0700

    minor dropdown update

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CustomButton from './components/Button';
-import DropDown from './components/DropDown';
+import BottomDropDown from './components/BottomDropDown';
+
+function PropFunctionTest(arg) {
+  alert(arg)
+}
 
 function TestEnv() {
   return (
     <div className="App">
       <h1>Test Env</h1>
       <CustomButton
-        onClick={null}
+        onClick={PropFunctionTest}
         buttonText={"Test"}
       />
-      <DropDown
-        onSelect={null}
+      <BottomDropDown
+        handleSelect={PropFunctionTest}
         selectionList={["item1", "item2", "item3"]}
       />
     </div>
---
+++ src/components/BottomDropDown.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import './BottomDropDown.css';
+
+/*
+Expected Props:
+    selectionList = an array of items that go in the dropdown
+    handleSelect = a method from the parent to return the active selection
+*/
+
+export default class BottomDropDown extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeSelection: this.props.selectionList[0],
+            selectionList: this.props.selectionList,
+            menuOpen: false,
+        }
+
+        this.dropdownClicked = this.dropdownClicked.bind(this);
+        this.selectItem = this.selectItem.bind(this);
+    }
+
+    dropdownClicked() {
+        const menuOpen = this.state.menuOpen;
+        this.setState({
+            menuOpen: !menuOpen,
+        });
+    }
+
+    selectItem(selectedItem) {
+        const menuOpen = this.state.menuOpen;
+        console.log(selectedItem);
+        this.setState({
+            activeSelection: selectedItem,
+            menuOpen: !menuOpen,
+        });
+        this.props.handleSelect(selectedItem);
+    }
+    
+    render() {
+        console.log(this.state.activeSelection)
+        var expandedDropDown;
+        const clickedStatus = (this.state.menuOpen)? "\u25b2" : "\u25bc";
+        
+        if (this.state.menuOpen) {
+            expandedDropDown = this.state.selectionList.map(listItem => {
+                var id = null;
+                if (listItem === this.state.activeSelection) {
+                    id = "active";
+                }
+
+                return (
+                    <div className="list-item-box" id={id} key={listItem} onClick={() => this.selectItem(listItem)}>
+                        <p className="list-item-text">{listItem}</p>
+                    </div>
+                )
+            });
+        } else {
+            expandedDropDown = null;
+        }
+        return (
+            <div className="drop-down-container">
+                <p id="active-selection" onClick={this.dropdownClicked}><span>{this.state.activeSelection}</span></p>
+                {expandedDropDown}
+                <button id="dropdown-button" onClick={this.dropdownClicked}><span>{clickedStatus}</span></button>
+            </div>
+        )
+
+    }
+}
